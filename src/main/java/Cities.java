8e96f6aa9ae7b76769f51205c5c4d10464bf6e8d
Danny4life <osiki.segun@gmail.com>
2025-05-19 07:02:02 +0100

    edit file

--- src/main/java/Cities.java
+++ src/main/java/Cities.java
@@ -7,121 +7,88 @@ public class Cities {
     // this hold a collection of cities
     private List<City> cities = new ArrayList<>();
 
-
-    public void init(String fileName){
-
-        try(Scanner scanner = new Scanner(new File(fileName))){
-
-            scanner.useDelimiter("\\t/\\n");
-
-            while (scanner.hasNext()){
-
+    public void init(String filename) {
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            scanner.useDelimiter("\\t|\\n");
+            while (scanner.hasNext()) {
                 String name = scanner.next();
                 String country = scanner.next();
                 int population = Integer.parseInt(scanner.next());
                 double area = Double.parseDouble(scanner.next());
-
                 cities.add(new City(name, country, population, area));
-
             }
-
-        }catch (FileNotFoundException e){
-            System.out.println("File not found: " + fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
         }
-
     }
 
-    public void  listAlphabetically(){
+    public void listAlphabetically() {
         System.out.println("\nCity list\n");
-
         List<City> sorted = new ArrayList<>(cities);
         Collections.sort(sorted);
-
-        for(City city : sorted){
+        for (City city : sorted) {
             System.out.println(city);
-
         }
-
     }
 
-    public void listByCountry(String country){
+    public void listByCountry(String country) {
         System.out.println("\nCities in " + country + ":\n");
-
-        for(City city : cities){
-            if(city.getCountry().equals(country)){
+        for (City city : cities) {
+            if (city.getCountry().equals(country)) {
                 System.out.println(city);
-
             }
-
         }
     }
 
-    public void listByPopulation(){
+
+    public void listByPopulation() {
         System.out.println("\nCity list by population\n");
         List<City> sorted = new ArrayList<>(cities);
         sorted.sort(new PopulationComparator());
-
-        for(City city : sorted){
+        for (City city : sorted) {
             System.out.println(city);
-
         }
     }
 
-    public void listByDensity(){
+    public void listByDensity() {
         System.out.println("\nCity list by density\n");
-
         List<City> sorted = new ArrayList<>(cities);
         sorted.sort(new DensityComparator());
-
-        for(City city : sorted){
+        for (City city : sorted) {
             System.out.printf("%s density: %.0f\n", city, city.getDensity());
-
         }
     }
 
-
-    // the -> arrow key signify a lambda expression
-    // a lambda expression is a shorter of iterating over a large dataset
-    public Map<String, List<City>> mapByCountry(){
+    public Map<String, List<City>> mapByCountry() {
         Map<String, List<City>> map = new HashMap<>();
-
-        for(City city : cities){
+        for (City city : cities) {
             map.computeIfAbsent(city.getCountry(), k -> new ArrayList<>()).add(city);
-
         }
-
         return map;
     }
 
-    public void printMapByCountry(){
+    public void printMapByCountry() {
         Map<String, List<City>> map = mapByCountry();
-
-        for(String country : map.keySet()){
+        for (String country : map.keySet()) {
             System.out.println("\n" + country + ":");
-
-            for (City city : map.get(country)){
+            for (City city : map.get(country)) {
                 System.out.println(city);
-
             }
-
         }
     }
 
-    private static class PopulationComparator implements Comparator<City>{
-
+    // Inner Comparator classes
+    private static class PopulationComparator implements Comparator<City> {
         @Override
         public int compare(City c1, City c2) {
-            return Integer.compare(c2.getPopulation(),
-                    c1.getPopulation());
+            return Integer.compare(c2.getPopulation(), c1.getPopulation()); // descending
         }
     }
 
-    private static class DensityComparator implements Comparator<City>{
-
+    private static class DensityComparator implements Comparator<City> {
         @Override
         public int compare(City c1, City c2) {
-            return Double.compare(c2.getDensity(),
-                    c1.getDensity());
+            return Double.compare(c2.getDensity(), c1.getDensity()); // descending
         }
     }
 }
--- src/main/java/City.java
+++ src/main/java/City.java
@@ -1,11 +1,8 @@
 public class City implements Comparable<City> {
 
     private String name;
-
     private String country;
-
     private int population;
-
     private double area;
 
     public City(String name, String country, int population, double area) {
@@ -16,58 +13,22 @@ public class City implements Comparable<City> {
     }
 
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public String getCountry() { return country; }
+    public int getPopulation() { return population; }
+    public double getArea() { return area; }
 
-    public String getCountry() {
-        return country;
-    }
-
-    public int getPopulation() {
-        return population;
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public void setPopulation(int population) {
-        this.population = population;
-    }
-
-    public void setArea(double area) {
-        this.area = area;
-    }
-
-    public double getDensity(){
+    public double getDensity() {
         return population / area;
     }
 
-    // the compareTo method will allow us to compare names of different cities
     @Override
     public int compareTo(City other) {
         return this.name.compareTo(other.name);
     }
 
-
-
     @Override
     public String toString() {
-        return "City{" +
-                "name='" + name + '\'' +
-                ", country='" + country + '\'' +
-                ", population=" + population +
-                ", area=" + area +
-                '}';
+        return String.format("%s(%s) population: %d area: %.0f", name, country, population, area);
     }
 }
